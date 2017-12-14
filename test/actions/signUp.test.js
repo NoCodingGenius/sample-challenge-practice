e6ab91a37bae35a7c4fa8548ad3d0a66ffca4900
LaToya Williams <platinumtoii@yahoo.com>
2017-12-14 10:59:44 -0800

    Complete TEST.

--- test/actions/signUp.test.js
+++ test/actions/signUp.test.js
@@ -5,12 +5,12 @@ import { signUp } from '../../src/actions'
 describe('function signUp ', () => {
   it('should create a new row in the users table', () => {
     return signUp({
-      full_name: "JahriB",
-      email: "jahb@test.com",
+      full_name: "Jahri",
+      email: "jahriB@test.com",
       password: "12345"
     })
     .then((user) => {
-      expect(user.full_name).to.equal('JahriB')
+      expect(user[0].id).to.equal(1)
     })
   })
 })
